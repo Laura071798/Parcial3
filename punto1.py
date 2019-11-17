3ed5f8974731c5bcde11eb043b9eed3fc3fd9837
Laura <laura.ramirez2@correo.usa.edu.co>
2019-11-16 20:13:41 -0500

    primer punto completo

--- punto1.py
+++ punto1.py
@@ -19,7 +19,7 @@ a_2 = placa.get_pin('a:2:i')
 time.sleep(0.5)
 ventana = Tk()
 ventana.geometry('800x400')
-ventana.title("UI para sistemas de control")
+ventana.title("Parcial")
 
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate(r'C:/Users/laura/Documents/Herramientas2/Parcial/keys/key.json')
@@ -28,18 +28,18 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://parcial3-65f7c.firebaseio.com/'
 })
 
-marco1 = Frame(ventana, bg="gray", highlightthickness=1, width=1280, height=800, bd= 5)
-marco1.place(x = 0,y = 0)
-b=Label(marco1,text="")
+frame1 = Frame(ventana, bg="gray", highlightthickness=1, width=400, height=300, bd= 5)
+frame1.place(x = 200,y = 50)
+b=Label(frame1,text="")
 
 
 
 
-valor= Label(marco1, bg='sky blue', font=("Arial Bold", 15), fg="white", width=5)
+valor= Label(frame1, bg='mediumpurple1', font=("Arial Bold", 15), fg="white", width=5)
 adc_data=StringVar()
-valor2 = Label(marco1, bg='sky blue', font=("Arial Bold", 15), fg="white", width=5)
+valor2 = Label(frame1, bg='mediumpurple1', font=("Arial Bold", 15), fg="white", width=5)
 adc_data2=StringVar()
-valor3= Label(marco1, bg='sky blue', font=("Arial Bold", 15), fg="white", width=5)
+valor3= Label(frame1, bg='mediumpurple1', font=("Arial Bold", 15), fg="white", width=5)
 adc_data3=StringVar()
 
 
@@ -65,14 +65,14 @@ def adc_read():
 
 
 valor.configure(textvariable=adc_data)
-valor.place(x=130, y=90)
+valor.place(x=20, y=30)
 valor2.configure(textvariable=adc_data2)
-valor2.place(x=200, y=90)
+valor2.place(x=20, y=90)
 valor3.configure(textvariable=adc_data3)
-valor3.place(x=270, y=90)
+valor3.place(x=20, y=150)
 
-prom_15=Button(marco1,text="send",command=adc_read)
-prom_15.place(x=60, y=90)
+prom_15=Button(frame1,text="send",command=adc_read)
+prom_15.place(x=120, y=90)
 
 
 ventana.mainloop()
